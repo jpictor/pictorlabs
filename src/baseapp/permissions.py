1a4cb8f391fb19fee9f5be6d133a477f0b901785
Jay Painter <jay.painter@declara.com>
2016-09-26 10:36:24 -0700

    added new permissions class

--- src/baseapp/permissions.py
+++ src/baseapp/permissions.py
@@ -2,7 +2,12 @@ from rest_framework import permissions
 
 
 class InternalOrIsAuthenticated(permissions.BasePermission):
-
+    """
+    Permissions class which allows internal services to use the API without authorization.
+    Access using NGINX proxy.
+    The HTTP_IS_INTERNAL header should be added by NGINX internal proxy.
+    The header should be set to 0 with no forwarding for the Internet-facing proxy.
+    """
     def has_permission(self, request, view):
         if request.META.get('HTTP_IS_INTERNAL') == '1':
             return True
